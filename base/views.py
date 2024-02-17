ca2f2ab34c6a10063e6a179afdc9834799c69d75
FZywica <zywicafilip01@gmail.com>
2024-02-17 12:15:32 +0100

    I added more views

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
 from .forms import RoomForm
+from .forms import UserForm
 
 
 
@@ -22,9 +23,9 @@ def loginPage(request):
         try:
             user = User.objects.get(username=username)
         except:
-            messages.error(request,'User does not exist')
+            messages.error(request, 'User does not exist')
 
-        user = authenticate(request,username=username,password=password)
+        user = authenticate(request, username=username,password=password)
 
         if user is not None:
             login(request, user)
@@ -33,7 +34,7 @@ def loginPage(request):
             messages.error(request,'Username OR password does not exist')
 
 
-    context={'page': page}
+    context = {'page': page}
     return render(request,'base/login_register.html', context)
 
 
@@ -67,7 +68,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
     )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count=rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
     context= {'rooms':rooms,'topics':topics,
@@ -89,16 +90,33 @@ def room(request,pk):
     context = {'room': room, 'room_messages': room_messages,'participants':participants}
     return render(request, 'base/room.html',context)
 
+@login_required(login_url='login')
+def userProfile(request,pk):
+    user=User.objects.get(id=pk)
+    rooms= user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context={'user':user,'rooms':rooms,'room_messages':room_messages,'topics':topics}
+    return render(request,'base/profile.html',context)
+
 
 @login_required(login_url='login')
 def createRoom(request):
     form= RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context= {'form':form}
+        topic_name = request.POST['topic']
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+
+        Room.objects.create(
+            host = request.user,
+            topic = topic,
+            name = request.POST['name'],
+            description = request.POST['description'],
+        )
+
+        return redirect('home')
+    context= {'form':form,'topics':topics}
     return render(request, 'base/room_form.html', context)
 
 
@@ -106,16 +124,19 @@ def createRoom(request):
 def updateRoom(request,pk):
     room= Room.objects.get(id=pk)
     form= RoomForm(instance=room)
-
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse('Your are not allowed here!!')
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context={'form': form}
+        topic_name = request.POST['topic']
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST['name']
+        room.description = request.POST['description']
+        room.topic = topic
+        room.save()
+        return redirect('home')
+    context={'form': form,'topics':topics,'room': room}
     return render(request, 'base/room_form.html',context)
 
 @login_required(login_url='login')
@@ -143,4 +164,30 @@ def deleteMessage(request,pk):
         return redirect('home')
     return render(request,'base/delete.html',{'obj': message})
 
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
 
+    if request.method == "POST":
+        form = UserForm(request.POST,instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile',pk=user.id)
+
+    context = {'form': form}
+    return render(request,'base/update-user.html',context)
+
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    topics = Topic.objects.filter(name__icontains=q)
+    context= {'topics': topics}
+    return render(request, 'base/topics.html',context)
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    context={'room_messages': room_messages}
+    return render(request, 'base/activity.html',context)
--- django_project/settings.py
+++ django_project/settings.py
@@ -38,10 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
+
+    'rest_framework',
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+"corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,10 +123,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 STATIC_URL = 'static/'
+
+# STATIC_ROOT =
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True
